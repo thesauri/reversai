b88c36a53664ecaaabbf638830eabd1627b39859
Walter Berggren <walter.berggren@gmail.com>
2020-11-05 21:38:57 +0200

    Add websocket server and send board state

--- backend/reversi/reversi.py
+++ backend/reversi/reversi.py
@@ -1,11 +1,10 @@
-from game_logic import move, default_game_board, is_valid_move
-from helpers import print_board
+from .logic import move, default_game_board, is_valid_move
+from .helpers import print_board
 
-def play_game():
+def test_game():
     board = default_game_board()
     turn = "black"
 
-    print()
     board = move(board, turn, (2, 3))
     print_board(board)
 
@@ -20,3 +19,4 @@ def play_game():
     turn = "white"
     board = move(board, turn, (0, 3))
     print_board(board)
+    return board
