b35b715ee4b292ab5cd86176b7fdd49b231b78e7
Walter Berggren <walter.berggren@gmail.com>
2020-11-07 22:12:44 +0200

    Cleanup in sample bot

--- backend/reversi/bots/oth3ll0grindr2000.py
+++ backend/reversi/bots/oth3ll0grindr2000.py
@@ -15,13 +15,12 @@ class Oth3lloGrindr2000:
         self.name = "0th3ll0GRINDR2000"
         self.author = "wotlol u sammoa"
         self.color = color
-        pass
 
     def get_move(self, board):
         """
         board: The game board as a row-column array of "black", "white", or "", i.e. the cell board[3][4] refers to row three and column 4. The rows and columns are indexed from 0 (valid values 0-7).
 
-        Returns: The position to place the disc as a tuple (row_index, column_index), i.e. (7, 2) for row 7 and column 2. Note that the position *has* to be valid (otherwise the game server will throw an error). Use the is_valid_move from reversi/logic to test whether a move is valid or not.
+        Returns: The position to place the disc on as a tuple (row_index, column_index), i.e. (7, 2) for row 7 and column 2. Note that the position *has* to be valid (otherwise the game server will throw an error). Use the is_valid_move from reversi/logic to test whether a move is valid or not.
         """
         while True:
             row_index = random.randint(0, 7)
